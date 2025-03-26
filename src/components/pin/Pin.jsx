f82007b304e2e315533e5c8bb04e80732183c670
Aniesodo collins <aniesodoc@gmail.com>
2025-03-26 08:02:38 +0100

    21 commit

--- src/components/pin/Pin.jsx
+++ src/components/pin/Pin.jsx
@@ -2,21 +2,18 @@ import React from "react";
 import "./Pin.scss";
 import { Link } from "react-router-dom";
 import { Marker, Popup } from "react-leaflet";
-import L from "leaflet";
-import markerIcon from "leaflet/dist/images/marker-icon.png";
-import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
-import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
-// Fix marker icons to prevent issues after deployment
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: markerIcon2x,
-  iconUrl: markerIcon,
-  shadowUrl: markerShadow,
-});
+
 
 const Pin = ({ item }) => {
-  console.log(item);
+  const customIcon = new L.Icon({
+    iconUrl: markerIcon,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: markerShadow,
+    shadowSize: [41, 41],
+  });
 
   // Check if the first media is an image or video
   const firstMedia = item.images[0] || item.videos[0];
@@ -26,7 +23,7 @@ const Pin = ({ item }) => {
     firstMedia?.endsWith(".ogg");
 
   return (
-    <Marker position={[item.latitude, item.longitude]}>
+    <Marker position={[item.latitude, item.longitude]} icon={customIcon}>
       <Popup minWidth={250} maxWidth={300} autoPan={true}>
         <div className="popupContainer">
           {isVideo ? (
